d76eb632f163d4a691cfd49e5161cc934282ac9c
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 00:35:21 +0500

    Task1

---
+++ Tyuiu.SavchenkoEI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Егор";
+        int age = 24;
+        Console.WriteLine("Здравствуйте меня зовут "+ name + " мне " + age + " лет.");
+        Console.ReadKey();
+    }
+}
