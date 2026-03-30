ecbc6ac99a1bcf01789186502709dab539ed515f
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 01:11:56 +0500

    Task2

---
+++ Tyuiu.SavchenkoEI.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SavchenkoEI.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.SavchenkoEI.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Егор";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Егор", res);
+        }
+    }
+}
---
+++ Tyuiu.SavchenkoEI.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.SavchenkoEI.Sprint0.Task2.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Егор"));
+        Console.ReadKey();
+    }
+}
