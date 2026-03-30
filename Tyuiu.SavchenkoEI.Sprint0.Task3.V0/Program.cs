5f6f249db6be18cf042f6afd3e2fe8edcfeb61a3
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 01:32:40 +0500

    Task3

---
+++ Tyuiu.SavchenkoEI.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.SavchenkoEI.Sprint0.Task3.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7,7));
+        Console.ReadKey();
+    }
+}
